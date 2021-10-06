236060409875cb712ea52deabd1ba32cdb0f87c1
lilytao2001 <lily.tao0014@gmail.com>
2021-10-06 17:10:03 -0400

    add search bar

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -10,7 +10,7 @@ class CurrentWeather extends Component {
                 <div className="weather" >
                     <div className="city">{this.props.city}</div>
                     <div className="temp">{this.props.temp}°C</div>
-                    <div className="weather-high-low">H={this.props.max}°C L={this.props.min}°C</div>
+                    <div className="weather-high-low">H={this.props.max}°C   L={this.props.min}°C</div>
                     <div className="weather-high-low">{this.props.desc}</div>
                 </div>
             </React.Fragment>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import CityOptions from './CityOptions';
 import CurrentWeather from './CurrentWeather';
 import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
 import "./Styles.css";
 
 function Home() {
     const [apiData, setApiData] = useState({});
     const [getCity, setGetCity] = useState("Los Angeles");
+    const [text, setText] = useState("")
     const [bg, setBg] = useState("https://upload.wikimedia.org/wikipedia/commons/3/32/20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg");
 
     const apiKey = "83ebd1e319767f4fb23d26430aca217d";
@@ -21,7 +29,19 @@ function Home() {
 
     const inputHandler = (event) => {
         setGetCity(event.target.value);
-        switch (event.target.value) {
+        changeBg(event.target.value)
+        console.log(bg)
+    };
+
+    const textInputHandler = (event) => (
+        setText(event.target.value)
+    )
+    const handleSubmit = () => {
+        setGetCity(text); 
+        changeBg (text);
+    }
+    const changeBg = (city) =>{
+        switch (city) {
             case "Toronto":
                 setBg("https://static.onecms.io/wp-content/uploads/sites/28/2021/07/08/toronto-ontario-canada-lead-TORONTOTG0721.jpg")
                 break;
@@ -37,10 +57,12 @@ function Home() {
             case "New York":
                 setBg("https://media.cntraveler.com/photos/5a8f3b070e2cf839e9dbfa1d/2:1/w_2560%2Cc_limit/NYC_GettyImages-640006562.jpg")
                 break;
-        }
-        console.log(bg)
-    };
+            default:
+                setBg("https://images.unsplash.com/photo-1444723121867-7a241cacace9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80")
+   
 
+        }
+    }
     const convertToCelcius = (k) => {
         return (k - 273.15).toFixed(0);
     }
@@ -53,6 +75,13 @@ function Home() {
         width: '100vw',
         height: '100vh'
     }
+    const theme = createTheme({
+        palette: {
+          primary: {
+            main: "#ffffff",
+          },
+        },
+      });
     return ( 
         <React.Fragment>
             <div style={style} className="dark-mask">
@@ -62,7 +91,25 @@ function Home() {
                     getCity = {getCity}
                     setCity = {inputHandler}
                 />
-                </Container>        
+                </Container>      
+                <div className="input-container">
+                    <Stack direction="row" spacing={4} justifyContent="center" alignItems="center">
+                        <div className="input">Elsewhere?</div>
+                        <input
+                            id="text"
+                            className="text-input"
+                            type="text"
+                            id="location-name"
+                            onChange={textInputHandler}
+                            value={text}
+                        />
+                        <ThemeProvider theme={theme}>
+                            <IconButton aria-label="search" onClick ={handleSubmit}>
+                                <SearchIcon color="primary" sx={{ fontSize: 35 }}/>
+                            </IconButton>
+                        </ThemeProvider>
+                    </Stack>
+                </div>
                 {apiData.main ? (
                     <React.Fragment>
                         <CurrentWeather 
